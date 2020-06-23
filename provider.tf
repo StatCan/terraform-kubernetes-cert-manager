67977f3badb605d5a829f84a87b11f253c260366
Justin Bertrand <justin.bertrand@canada.ca>
2020-06-23 16:07:10 +0000

    feat(helm3): update required_providers syntax.

--- provider.tf
+++ provider.tf
@@ -1,11 +1,6 @@
 terraform {
   required_providers {
-    helm = {
-      version = ">= 1.0.0"
-    }
-
-    kubernetes = {
-      version = ">= 1.10.0"
-    }
+    helm = ">= 1.0.0"
+    kubernetes = ">= 1.10.0"
   }
 }
