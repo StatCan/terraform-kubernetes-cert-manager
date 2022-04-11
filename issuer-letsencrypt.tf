4e4d32fe2dec48f95ebc887e60804702db321bd6
Adam Glavine <adam.glavine@cloud.statcan.ca>
2022-04-11 17:16:47 +0000

    feat-merge-cert-manager-tfk8s-changes

---
+++ issuer-letsencrypt.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_manifest" "clusterissuer_letsencrypt" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "letsencrypt"
+    }
+    "spec" = {
+      "acme" = {
+        "email"          = var.letsencrypt_email
+        "preferredChain" = ""
+        "privateKeySecretRef" = {
+          "name" = "letsencrypt"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "dns01" = {
+              "azureDNS" = {
+                "hostedZoneName"    = var.azure_zone_name
+                "resourceGroupName" = var.azure_resource_group_name
+                "subscriptionID"    = var.azure_subscription_id
+              }
+            }
+            "selector" = {
+              "matchLabels" = {
+                "use-azuredns-solver" = "true"
+              }
+            }
+          },
+        ]
+      }
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "cert_manager" {
   repository          = var.helm_repository
   repository_username = var.helm_repository_username
   repository_password = var.helm_repository_password
-  chart               = "cert-manager"
+  chart               = var.chart_name
   version             = "v${var.chart_version}"
   namespace           = var.helm_namespace
 
@@ -15,48 +15,4 @@ resource "helm_release" "cert_manager" {
   values = [
     var.values,
   ]
-}
-
-resource "local_file" "issuer_letsencrypt_staging" {
-  content = templatefile("${path.module}/config/issuer-letsencrypt-staging.yaml", {
-    letsencrypt_email         = var.letsencrypt_email
-    azure_subscription_id     = var.azure_subscription_id
-    azure_resource_group_name = var.azure_resource_group_name
-    azure_zone_name           = var.azure_zone_name
-  })
-
-  filename = "${path.module}/issuer-letsencrypt-staging.yaml"
-}
-
-
-resource "null_resource" "issuer_letsencrypt_staging" {
-  triggers = {
-    hash = sha256(local_file.issuer_letsencrypt_staging.content)
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${local_file.issuer_letsencrypt_staging.filename}"
-  }
-}
-
-resource "local_file" "issuer_letsencrypt" {
-  content = templatefile("${path.module}/config/issuer-letsencrypt.yaml", {
-    letsencrypt_email         = var.letsencrypt_email
-    azure_subscription_id     = var.azure_subscription_id
-    azure_resource_group_name = var.azure_resource_group_name
-    azure_zone_name           = var.azure_zone_name
-  })
-
-  filename = "${path.module}/issuer-letsencrypt.yaml"
-}
-
-
-resource "null_resource" "issuer_letsencrypt" {
-  triggers = {
-    hash = sha256(local_file.issuer_letsencrypt.content)
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${local_file.issuer_letsencrypt.filename}"
-  }
 }
